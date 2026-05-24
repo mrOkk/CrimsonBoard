452bdaf30a7bf3fdf42acc297f8e97ed4a8a4ff3
Oleg Koposov <xtrimsmile@gmail.com>
2026-05-24 22:26:47 +0300

    08 Add tile coord helpers, CurrentCell and DirectionIndicator

--- CB-client/Assets/Scripts/Entities/PlayerView.cs
+++ CB-client/Assets/Scripts/Entities/PlayerView.cs
@@ -6,8 +6,10 @@ namespace CrimsonBoard
     {
         [SerializeField] private Transform _weaponLocator;
         [SerializeField] private HealthComponent _health;
+        [SerializeField] private Transform _directionIndicator;
 
         public Transform WeaponLocator => _weaponLocator;
         public HealthComponent Health => _health;
+        public Transform DirectionIndicator => _directionIndicator;
     }
 }
